ea7cbb4d615b2eba5fd4592bfa3f61e56d398174
Alexey Savinov <belsat.net@gmail.com>
2017-09-23 21:41:30 +0300

    realize check code

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,83 @@
+import re
+import sys
 
 
-def get_password_strength(password):
-    pass
+def load_data(filepath):
+    with open(filepath, "r", encoding='utf-8') as input_file:
+        raw_text = input_file.read()
+    return raw_text
+
+
+def check_password_not_in_file(user_password, password_string):
+    if password_string.find(user_password) == -1:
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def check_password_length(user_password, min_length, max_length):
+    if len(user_password) in range(min_length, max_length):
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def check_password_has_digit(user_password):
+    if re.search(r'[0-9]', user_password):
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def check_password_has_upper(user_password):
+    if re.search(r'[A-Z]', user_password):
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def check_password_has_lower(user_password):
+    if re.search(r'[a-z]', user_password):
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def check_password_has_symbol(user_password):
+    if re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', user_password):
+        password_check = True
+    else:
+        password_check = False
+    return password_check
+
+
+def get_password_strength_score():
+    score = 1
+    min_length = 6
+    max_length = 12
+    check_list = [check_password_not_in_file(user_password, raw_data),
+                  check_password_length(user_password, min_length, max_length),
+                  check_password_has_digit(user_password),
+                  check_password_has_upper(user_password),
+                  check_password_has_lower(user_password),
+                  check_password_has_symbol(user_password)
+                  ]
+    for check in check_list:
+        if check:
+            score = score + 1.5
+    return score
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 2:
+        user_password = sys.argv[1]
+        filename = sys.argv[2]
+        raw_data = load_data(filename)
+        print('You password strength score : ' + str(get_password_strength_score()))
+    else:
+        print('No necessary data provided')
